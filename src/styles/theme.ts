83b5926c60a2de9ddec436c5fc53c11298c93a49
PYO SEUNGYEON <pyo.seungyeonn@gmail.com>
2023-05-02 17:16:04 +0900

    💄 공통 theme 파일 색상 변수 설정

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,5 +1,29 @@
 import { DefaultTheme } from 'styled-components';
 
+const color = {
+  navbar: '#1c1c1c',
+  navbarText: '#d6d5da',
+  navActiveText: '#ffffff',
+  navbarActive: '#494f5f',
+  userTag: '#62c355',
+  button01: '#0675e5',
+  status01: '#bbbbbb',
+  status02: '#f26262',
+  status03: '#76c891',
+  text: '#333333',
+  noticeText: '#f26262',
+  buttonText: '#ffffff',
+  background: '#ffffff',
+  inputBorder: '#e4e3e8',
+  placeholder: '#8a8a8a',
+  btnInActive: '#cbccd7',
+  btnActiveBackground: '#eff3ff',
+  buttonActive: '#014de3',
+  btnInActiveBackground: '#ffffff',
+  adminTag: '#7939cc',
+  masterTag: '#ff5252'
+};
+
 export const theme: DefaultTheme = {
-  color: {}
+  color
 };
