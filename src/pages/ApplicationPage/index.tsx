5267e2be8e43c1c0509a97dbd537e5dd75948af2
yousunzoo <dpfvmffjqm@gmail.com>
2023-05-02 16:55:33 +0900

    :sparkles: Router 경로 설정

---
+++ src/pages/ApplicationPage/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ApplicationPage() {
+  return <div>ApplicationPage</div>;
+}
+
+export default ApplicationPage;
---
+++ src/pages/ViewSchedulePage/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ViewSchedulePage() {
+  return <div>ViewSchedulePage</div>;
+}
+
+export default ViewSchedulePage;
