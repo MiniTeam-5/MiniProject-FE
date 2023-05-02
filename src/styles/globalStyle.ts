decbb8f440e4f5f3b369379438239ff21fae2fa8
yousunzoo <dpfvmffjqm@gmail.com>
2023-05-02 17:31:06 +0900

    :lipstick: pretendard 폰트 수정

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -25,8 +25,7 @@ a {
   text-decoration: none;
   color:inherit;
 }
-body {
-  font-family: 'Pretendard', sans-serif;
+body {font-family: "Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
   background-color: #ddd
 }
 `;
