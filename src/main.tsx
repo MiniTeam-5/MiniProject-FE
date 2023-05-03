d13ee272083880a7891acc41b33fe40886e6b6e1
yousunzoo <dpfvmffjqm@gmail.com>
2023-05-03 22:08:39 +0900

    :sparkles: msw 기반으로 mockup server 구현

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import { worker } from './mocks/broswer';
+
+if (process.env.NODE_ENV === 'development') {
+  worker.start();
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
