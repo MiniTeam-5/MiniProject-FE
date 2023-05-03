7a1db7ae2a216a907b23ac3601fe2cfb52ac7186
yong8048 <water8048@naver.com>
2023-05-03 15:45:38 +0900

    :sparkles: feat: 404ERROR page 작성

--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import * as S from './index.style';
 
 function NotFoundPage() {
-  return <div>NotFoundPage</div>;
+  return (
+    <S.NotFoundContainer>
+      <S.NotFoundDiv>
+        <S.NotFoundImg src='../../../public/assets/404error.jpg' alt='' />
+        <S.NotFoundFirP>죄송합니다. 현재 찾을 수 없는 페이지를 요청 하셨습니다.</S.NotFoundFirP>
+        <S.NotFoundSecP>
+          존재하지 않는 주소를 입력하셨거나, 요청하신 페이지는 주소가 변경, 삭제되어 찾을 수 없습니다.
+        </S.NotFoundSecP>
+        <S.NotFoundA href='/'>HOME</S.NotFoundA>
+      </S.NotFoundDiv>
+    </S.NotFoundContainer>
+  );
 }
 
 export default NotFoundPage;
