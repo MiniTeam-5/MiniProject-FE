0e975114231f0d6edce153e9d22b4edf012b5d46
PYO SEUNGYEON <pyo.seungyeonn@gmail.com>
2023-05-04 13:41:31 +0900

    ✨ ScheduleCalendar 컴포넌트 생성 및 HomePage 컴포넌트에 연결
    + 목업데이터 기반 캘린더 이벤트 렌더링

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,10 +1,16 @@
 import UserMain from '../../components/UserMain';
+import ScheduleCalendar from '../../components/common/ScheduleCalendar';
 import temp from '../../mockup/user_login.json';
 
 function HomePage() {
   const user = temp.data[0].role;
 
-  return <div>{user === 'USER' ? <UserMain /> : <>관리자 메인</>}</div>;
+  return (
+    <div>
+      {user === 'USER' ? <UserMain /> : <>관리자 메인</>}
+      <ScheduleCalendar />
+    </div>
+  );
 }
 
 export default HomePage;
