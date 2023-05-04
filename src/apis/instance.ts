4f6d900924d4290ef4b5b36d03d289dab316b085
yousunzoo <dpfvmffjqm@gmail.com>
2023-05-04 19:20:59 +0900

    Revert "Merge pull request #14 from pyozz/develop"
    This reverts commit 1d2e774b40f96c8c00547aee614ffecb33db72fa, reversing
    changes made to b3a6e6bc232b1ec9046a0bc2e576b94fc96e8de6.

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -1,30 +1,29 @@
-import axios, { AxiosRequestConfig, AxiosError } from 'axios';
+import axios, { AxiosError, AxiosRequestConfig } from 'axios';
 import { getCookie } from '../utils/cookies';
 
-const getAxiosInstance = (option?: { multi?: boolean }) => {
+const getAxiosInstance = () => {
   const config: AxiosRequestConfig = {
-    baseURL: 'http://moodlog-env.eba-a73kg2hr.ap-northeast-2.elasticbeanstalk.com/',
+    baseURL: '/',
+    timeout: 3000,
     headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
+      'Content-Type': 'application/json'
     },
     withCredentials: true
   };
+
   const instance = axios.create(config);
-  instance.defaults.timeout = 3000;
-  //요청보낼 때 쿠키에있는 엑세스토큰을 가져와서 헤더에 셋
   instance.interceptors.request.use(
     (request) => {
       const token = getCookie('accessToken');
-      if (token) request.headers['Authorization'] = `Bearer ${token}`;
-      if (option && option.multi) request.headers['Content-Type'] = 'multipart/form-data';
+      if (token) request.headers['Authorization'] = 'Bearer' + token;
       return request;
     },
     (error: AxiosError) => {
-      console.log(error);
       return Promise.reject(error);
     }
   );
+
   return instance;
 };
-export const axiosInstance = getAxiosInstance;
+
+export const axiosInstance = getAxiosInstance();
