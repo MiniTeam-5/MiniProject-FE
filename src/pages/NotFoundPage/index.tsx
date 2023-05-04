cbbee6a276b665f9bb0914af4d331f62fbb9067f
yong8048 <water8048@naver.com>
2023-05-04 17:17:11 +0900

    :lipstick: feat: 에러페이지 수정

--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -5,7 +5,7 @@ function NotFoundPage() {
   return (
     <S.NotFoundContainer>
       <S.NotFoundDiv>
-        <S.NotFoundImg src='../../../public/assets/404error.jpg' alt='404error' />
+        <S.NotFoundImg src='../../../public/assets/404error.jpg' alt='error' />
         <S.NotFoundFirP>죄송합니다. 현재 찾을 수 없는 페이지를 요청 하셨습니다.</S.NotFoundFirP>
         <S.NotFoundSecP>
           존재하지 않는 주소를 입력하셨거나, 요청하신 페이지는 주소가 변경, 삭제되어 찾을 수 없습니다.
