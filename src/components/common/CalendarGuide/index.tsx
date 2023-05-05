e4146f679f0b048228a64ceccf7e71bd1991ca57
PYO SEUNGYEON <pyo.seungyeonn@gmail.com>
2023-05-05 22:51:18 +0900

    ♻️ 단일 태그로 코드 수정

--- src/components/common/CalendarGuide/index.tsx
+++ src/components/common/CalendarGuide/index.tsx
@@ -4,12 +4,12 @@ function CalendarGuide() {
   return (
     <S.Guide>
       <S.GuideCenter>
-        <S.AnuualColorBox></S.AnuualColorBox>
+        <S.AnuualColorBox />
         <span>연차</span>
       </S.GuideCenter>
 
       <S.GuideCenter>
-        <S.DutyColorBox></S.DutyColorBox>
+        <S.DutyColorBox />
         <span>당직</span>
       </S.GuideCenter>
     </S.Guide>
