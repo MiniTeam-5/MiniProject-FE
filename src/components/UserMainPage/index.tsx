cc1b9f746274128d5375d1df0d5dffe35e39d2f6
PYO SEUNGYEON <pyo.seungyeonn@gmail.com>
2023-05-08 02:45:19 +0900

    ✨ 지난 날의 신청 현황은 렌더링 않도록 구현

--- src/components/UserMainPage/index.tsx
+++ src/components/UserMainPage/index.tsx
@@ -3,19 +3,23 @@ import temp from '../../mockup/schedule_login.json';
 import * as S from './styles';
 
 function UserMainPage() {
-  function sortByStartDate(a: { start_date: string }, b: { start_date: string }): number {
-    const dateA = a['start_date'] ? new Date(a['start_date']).getTime() : 0;
-    const dateB = b['start_date'] ? new Date(b['start_date']).getTime() : 0;
-    return dateA < dateB ? 1 : -1;
-  }
+  // function sortByStartDate(a: { start_date: string }, b: { start_date: string }): number {
+  //   const dateA = a['start_date'] ? new Date(a['start_date']).getTime() : 0;
+  //   const dateB = b['start_date'] ? new Date(b['start_date']).getTime() : 0;
+  //   return dateA < dateB ? 1 : -1;
+  // }
 
-  const annualList = temp.data.filter((item) => item.type === 'annual').sort(sortByStartDate);
-  const dutyList = temp.data.filter((item) => item.type === 'duty').sort(sortByStartDate);
+  const annualList = temp.data.filter((item) => item.type === 'annual');
+  const dutyList = temp.data.filter((item) => item.type === 'duty');
+
+  const today = new Date().toLocaleString('ko-KR', { timeZone: 'Asia/Seoul' }).slice(0, 10);
+  const filteredAnnualList = annualList.filter((item) => new Date(item.start_date) >= new Date(today));
+  const filteredDutyList = dutyList.filter((item) => new Date(item.start_date) >= new Date(today));
 
   return (
     <S.UserMain>
-      <ApplicationStatus title='연차 신청 현황' annualList={annualList} />
-      <ApplicationStatus title='당직 신청 현황' dutyList={dutyList} />
+      <ApplicationStatus title='연차 신청 현황' annualList={filteredAnnualList} />
+      <ApplicationStatus title='당직 신청 현황' dutyList={filteredDutyList} />
     </S.UserMain>
   );
 }
