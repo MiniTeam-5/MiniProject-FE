8da55df610121273fdaf2abfb575844cfc7c407b
yong8048 <water8048@naver.com>
2023-05-08 22:26:19 +0900

    feat: LoginPage UI 제작

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,7 +1,64 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import * as S from './styles';
+import { useState } from 'react';
 
 function LoginPage() {
-  return <div>LoginPage</div>;
+  const [isChecked, setIsChecked] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const ChangeCheckbox = () => {
+    setIsChecked(!isChecked);
+  };
+
+  const Login = () => {};
+
+  return (
+    <S.Login>
+      <S.LoginContainer>
+        <S.LoginLogo src='../../../public/assets/logo.png' alt='logo' />
+        <S.LoginWelcomeP>Welcom Back!</S.LoginWelcomeP>
+        <S.LoginForm>
+          <div className='login_email'>
+            <p>Email</p>
+            <S.LoginInput
+              type='text'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder='이메일'
+              className='login_email'
+            />
+          </div>
+
+          <div className='login_password'>
+            <p>Password</p>
+            <S.LoginInput
+              type='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder='비밀번호'
+              className='login_password'
+            />
+          </div>
+
+          <div className='login_checkbox' onClick={ChangeCheckbox}>
+            <input type='checkbox' checked={isChecked} onChange={ChangeCheckbox} className='saveEmail' />
+            <p>이메일 주소 기억하기</p>
+          </div>
+
+          <S.LoginBtn onClick={Login}>로그인 하기</S.LoginBtn>
+
+          <div className='login_signup'>
+            <span>아직 계정이 없으신가요?</span>
+            <Link to={'/signup'}>
+              <p className='link_to_signup'>회원가입</p>
+            </Link>
+          </div>
+        </S.LoginForm>
+      </S.LoginContainer>
+      <S.LoginBanner src='../../../public/assets/banner01.png' alt='Banner' />
+    </S.Login>
+  );
 }
 
 export default LoginPage;
