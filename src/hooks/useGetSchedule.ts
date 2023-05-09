1053261238cfab38d97d73d19789d0ef1293dd51
yousunzoo <dpfvmffjqm@gmail.com>
2023-05-09 14:26:50 +0900

    :sparkles: useLocation 활용하여 내 일정보기 페이지에서 보여질 데이터 추출
    내 일정 보기 페이지에서는 내 연차 + 내 당직 정보만 보여짐

--- src/hooks/useGetSchedule.ts
+++ src/hooks/useGetSchedule.ts
@@ -2,21 +2,23 @@ import { useQuery } from 'react-query';
 import { getSchedules } from '../apis/auth';
 import { IUseScheduleQuery } from '../interfaces/common';
 import { AxiosError } from 'axios';
+import { useLocation } from 'react-router-dom';
 
 function useGetSchedule(select?: 'annual' | 'duty') {
   const { data, isLoading, error } = useQuery<IUseScheduleQuery, AxiosError>('schedule', getSchedules);
   const userId = 1; // store에서 가져오기
+  const { pathname } = useLocation();
   if (!data?.data) return { data: [], isLoading, error };
 
   const schedules = data['data'];
   // annual -> 모든 사람 연차 + 내 당직
   // duty -> 내 연차 + 내 당직
   const annualList = schedules.filter((item) => {
-    if (select === 'duty') return item.type === 'annual' && item.id === userId;
+    if (select === 'duty' || pathname === '/viewSchedule') return item.type === 'annual' && item.id === userId;
     return item.type === 'annual';
   });
   const dutyList = schedules.filter((item) => {
-    if (select) return item.type === 'duty' && item.id === userId;
+    if (select || pathname === '/viewSchedule') return item.type === 'duty' && item.id === userId;
     return item.type === 'duty';
   });
 
