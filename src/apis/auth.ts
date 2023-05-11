1f9cad04102429385a4b44200052c084454bc329
yousunzoo <dpfvmffjqm@gmail.com>
2023-05-12 00:10:17 +0900

    :sparkles: 연차 신청 버튼 활성화
    성공하면 홈 화면으로 돌아감

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,6 +1,12 @@
+import { IApplySchedule } from '../interfaces/common';
 import { axiosInstance } from './instance';
 
 export const getSchedules = async (date: string) => {
   const response = await axiosInstance().get(`/auth/leave/month/${date}`);
+  return response.data;
+};
+
+export const applySchedule = async (data: IApplySchedule) => {
+  const response = await axiosInstance().post('/auth/leave/apply', data);
   return response.data;
 };
