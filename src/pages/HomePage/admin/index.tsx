70f141a9bd1e2f78f20c11417c13cb3f2b73ba2e
jiyoon29 <yoon__29@naver.com>
2023-05-11 20:19:35 +0900

    ✨ state 전달

--- src/pages/HomePage/admin/index.tsx
+++ src/pages/HomePage/admin/index.tsx
@@ -1,13 +1,17 @@
+import { useState } from 'react';
 import ApplicationSatus from '../../../components/AdminHomePage/ApplicationStatus';
 import ApplicationRight from '../../../components/AdminHomePage/ApplicationRight';
+import { Leave } from '../../../interfaces/applicationStatus';
 import * as S from './styles';
 
 function Admin() {
+  const [leaveList, setLeaveList] = useState<Leave[]>([]);
+
   return (
     <S.AdminHome>
       <div className='flex'>
-        <ApplicationSatus />
-        <ApplicationRight />
+        <ApplicationSatus leaveList={leaveList} setLeaveList={setLeaveList} />
+        <ApplicationRight leaveList={leaveList} />
       </div>
     </S.AdminHome>
   );
