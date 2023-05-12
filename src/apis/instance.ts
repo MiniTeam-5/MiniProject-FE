6c4fd34d09aafce73e18d974923fe6ce2a92f2f6
jiyoon29 <yoon__29@naver.com>
2023-05-13 00:25:01 +0900

    ✨ 실제 서버 연결 - token 값은 받아와서 이미로 넣어줌

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -3,7 +3,8 @@ import { getCookie } from '../utils/cookies';
 
 const getAxiosInstance = (option?: { multi?: boolean }) => {
   const config: AxiosRequestConfig = {
-    baseURL: '/',
+    // baseURL: '/',
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*'
@@ -15,8 +16,11 @@ const getAxiosInstance = (option?: { multi?: boolean }) => {
   //요청보낼 때 쿠키에있는 엑세스토큰을 가져와서 헤더에 셋
   instance.interceptors.request.use(
     (request) => {
-      const token = getCookie('accessToken');
-      if (token) request.headers['Authorization'] = `Bearer ${token}`;
+      // const token = getCookie('accessToken');
+      // if (token)
+      request.headers[
+        'Authorization'
+      ] = `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJqd3RzdHVkeSIsInJvbGUiOiJST0xFX01BU1RFUiIsImlkIjo1LCJleHAiOjE2ODM5MjA4OTl9.1Sb2qjHg7yBf43po4FO1aFFL0Cjt55_S5lNoDP5FkGRkW8d4RQvLc9tHXUuIjbCUVeFjX-hZc-yZfezHFLzQpg`;
       if (option && option.multi) request.headers['Content-Type'] = 'multipart/form-data';
       return request;
     },
