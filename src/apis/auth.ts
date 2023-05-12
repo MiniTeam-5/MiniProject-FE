4438fd39e7ed0f779cd06272463c6c81ae699f99
yong8048 <water8048@naver.com>
2023-05-12 21:51:44 +0900

    feat: login 실패 조건 통합

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -14,9 +14,10 @@ export const applySchedule = async (data: IApplySchedule) => {
 
 export const login = async ({ email, password }: { email: string; password: string }) => {
   const response = await axiosInstance().post('/login', { email, password });
-  if (response.status.toString() === '400') return Error(response.data);
-  else if (response.status.toString() === '401') return Error(response.data);
-  else return response;
+
+  // if (response.status.toString() === '400' || response.status.toString() === '401') return Error(response.data);
+
+  return response;
 };
 
 export const signup = async (item: ISignup) => {
