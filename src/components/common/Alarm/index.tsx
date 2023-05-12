f008cefcc8c46f6981c6a3c52f87975ac43f7f67
yousunzoo <dpfvmffjqm@gmail.com>
2023-05-12 11:53:43 +0900

    :lipstick: 알림 컴포넌트 스타일 작업

--- src/components/common/Alarm/index.tsx
+++ src/components/common/Alarm/index.tsx
@@ -6,9 +6,15 @@ function Alarm({ handleCloseAlarm }) {
   // store에서 해당 유저의 알림 정보 가져오기
   return (
     <S.AlarmList>
+      <S.Title>알림 목록</S.Title>
       <S.CloseBtn onClick={handleCloseAlarm}>
         <MdClose />
       </S.CloseBtn>
+      <S.AlarmUl>
+        <S.AlarmLi>
+          <p>연차 신청이 승인되었습니다.</p>
+        </S.AlarmLi>
+      </S.AlarmUl>
     </S.AlarmList>
   );
 }
--- src/components/common/Alarm/styles.ts
+++ src/components/common/Alarm/styles.ts
@@ -6,11 +6,16 @@ export const AlarmList = styled.div`
   left: 320px;
   width: 300px;
   height: 400px;
+  padding: 16px;
   background-color: white;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
   border-radius: 10px;
 `;
 
+export const Title = styled.p`
+  color: ${({ theme }) => theme.color.text};
+  margin-bottom: 20px;
+`;
 export const CloseBtn = styled.button`
   position: absolute;
   top: 10px;
@@ -20,3 +25,7 @@ export const CloseBtn = styled.button`
   padding: 0;
   font-size: 24px;
 `;
+
+export const AlarmUl = styled.ul``;
+
+export const AlarmLi = styled.li``;
