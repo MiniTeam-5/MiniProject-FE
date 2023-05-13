e6eb01b1a2d17d0437d6cff7224a1af389c844eb
yong8048 <water8048@naver.com>
2023-05-13 12:28:52 +0900

    feat: 로그인후 메인페이지로 이동 구현

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -3,15 +3,21 @@ import { getUserData, login } from '../apis/auth';
 import { setCookie } from '../utils/cookies';
 import { useDispatch } from 'react-redux';
 import loginedUser, { userLogin } from '../store/reducers/userReducers';
+import { useNavigate } from 'react-router-dom';
+import store from '../store';
 
 export const useLogin = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   const { mutate } = useMutation(login, {
     onSuccess: async (data) => {
       // 로그인 성공 시 처리할 코드
       setCookie('accessToken', data.headers.authorization);
       const userData = await getUserData();
       dispatch(userLogin(userData.data.data));
+
+      navigate('/');
     }
   });
 
