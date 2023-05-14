bd99407f6548993b6126be0e113ecdee7c9cf851
yousunzoo <dpfvmffjqm@gmail.com>
2023-05-14 14:41:55 +0900

    :recycle: 변경된 api 응답에 맞게 코드 수정

--- src/hooks/useGetNewAlarms.ts
+++ src/hooks/useGetNewAlarms.ts
@@ -9,7 +9,6 @@ export const useGetNewAlarms = () => {
   const { data, error, isLoading } = useQuery('prevAlarms', getAlarms);
   const { id } = useSelector((state: IRootState) => state.loginedUser);
   const { data: prevAlarms } = useAlarm();
-  console.log(prevAlarms);
   const alarmList: IAlarm[] | undefined = data?.data;
   // 이전 데이터가 없을 때, 즉 로그인을 처음한 유저일 때 alarmList 전체 출력
   // 이전 데이터가 있으면, 이전 데이터와 비교해서 새로운 데이터만 출력
