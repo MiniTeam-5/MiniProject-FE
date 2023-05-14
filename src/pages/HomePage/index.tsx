6f178634f35a754203b8c33b3d3198257e8a40be
jiyoon29 <yoon__29@naver.com>
2023-05-14 21:54:43 +0900

    ✨ 데이터 받아와서 유저정보따라 변경

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,16 +1,19 @@
+import { useSelector } from 'react-redux';
 import Header from '../../components/common/Header';
 import UserMain from '../../components/UserMainPage';
 import ScheduleCalendar from '../../components/common/ScheduleCalendar';
 import Admin from './admin';
 import temp from '../../mockup/user_login.json';
+import { RootState } from '../../store';
 
 function HomePage() {
-  const user = temp.data[1].role;
+  // 유저 가져오기
+  const loginedUser = useSelector((state: RootState) => state.loginedUser);
 
   return (
     <div>
       <Header />
-      {user === 'USER' ? <UserMain /> : <Admin />}
+      {loginedUser.role === 'ROLE_USER' ? <UserMain /> : <Admin />}
       <ScheduleCalendar />
     </div>
   );
