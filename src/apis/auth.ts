7d9ddcca30537c5fc405a172540d3a715f339002
jiyoon29 <yoon__29@naver.com>
2023-05-14 15:03:04 +0900

    🐛 에러메시지만 안나오게 수정 - 다른분이 작업한거 받아올 예정

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,6 +1,6 @@
 import { axiosInstance } from './instance';
 
 export const getSchedules = async () => {
-  const response = await axiosInstance().get('/auth/leave');
+  const response = await axiosInstance().get('/auth/leave/all');
   return response.data;
 };
