a4aa0c4c87a58fa409fe844bca016ac07290129f
PYO SEUNGYEON <pyo.seungyeonn@gmail.com>
2023-05-14 18:37:06 +0900

    ♻️ refetch 버튼의 이벤트 핸들러 함수 생성 및 직급에 따른 기능 분기 처리

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -2,21 +2,25 @@ import { useNavigate } from 'react-router-dom';
 import * as S from './styles';
 import { GrPowerReset } from 'react-icons/gr';
 import { useQueryClient } from 'react-query';
+import { useSelector } from 'react-redux';
 
 function Header() {
   const navigate = useNavigate();
   const queryClient = useQueryClient();
+  const user = useSelector((state) => state.loginedUser.role);
+
+  const refetchHandler = () => {
+    if (user === 'ROLE_USER') {
+      queryClient.refetchQueries('schedule');
+      queryClient.refetchQueries('schedules');
+    }
+  };
 
   return (
     <S.Header>
       <S.Logo src='/assets/logo-blue.png' alt='lupin' />
       <S.Buttons>
-        <S.ResetBtn
-          onClick={() => {
-            queryClient.refetchQueries('schedule');
-            queryClient.refetchQueries('schedules');
-          }}
-        >
+        <S.ResetBtn onClick={refetchHandler}>
           새로고침
           <GrPowerReset />
         </S.ResetBtn>
