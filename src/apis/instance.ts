19fed938ed46a59c86514c4e9783336789773b5b
yousunzoo <dpfvmffjqm@gmail.com>
2023-05-14 19:31:03 +0900

    :bug: 배포 시 프로토콜 관련 문제 수정

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -3,8 +3,7 @@ import { getCookie } from '../utils/cookies';
 
 const getAxiosInstance = (option?: { multi?: boolean; refresh?: boolean }) => {
   const config: AxiosRequestConfig = {
-    baseURL: import.meta.env.VITE_API_URL,
-    //baseURL: '/',
+    baseURL: '/api',
     headers: {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*'
