ea914375d5c7c7d4acfbfbdde4a078030deb015a
yousunzoo <dpfvmffjqm@gmail.com>
2023-05-16 01:00:26 +0900

    :sparkles: 홈화면 로딩 시 로딩 컴포넌트 출력

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -4,11 +4,13 @@ import UserMain from '../../components/UserMainPage';
 import ScheduleCalendar from '../../components/common/ScheduleCalendar';
 import Admin from './admin';
 import { RootState } from '../../store';
+import Loading from '../../components/common/Loading';
 
 function HomePage() {
   // 유저 가져오기
   const user = useSelector((state: RootState) => state.loginedUser.role);
 
+  if (user === '') return <Loading />;
   return (
     <div>
       <Header />
