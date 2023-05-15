5395c44ea389964ccbf8ee918193936b9dad64f2
yousunzoo <dpfvmffjqm@gmail.com>
2023-05-15 23:18:55 +0900

    :bug: 타입스크립트 관련 에러 수정

--- src/hooks/useSignup.ts
+++ src/hooks/useSignup.ts
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 export const useSignup = () => {
   const navigate = useNavigate();
   const { mutate } = useMutation(signup, {
-    onSuccess: (data) => {
+    onSuccess: () => {
       // 회원가입 성공 시 처리할 코드
       alert('회원가입이 완료되었습니다!');
       navigate('/login');
